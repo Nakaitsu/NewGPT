7e0a986bc28cd39a24a38540f7d5e8db393531e1
Nakaitsu <vitoremail.66@gmail.com>
2023-02-24 18:07:46 -0300

    criado editor e integracao com o server

---
+++ front/public/index.js
@@ -0,0 +1,36 @@
+window.onload = () => {
+  // START EDITOR
+  const codeEditor = CodeMirror.fromTextArea(document.getElementById('code'), {
+    mode: 'javascript',
+    theme: 'ayu-dark',
+    lineNumbers: true,
+    autoCloseBrackets: true
+  })
+
+  // HANDLE COPY
+  document.getElementById('btn-copy').addEventListener('click', async (event) => {
+    event.preventDefault()
+    const code = codeEditor.getValue()
+
+    await navigator.clipboard.writeText(code)
+  })  
+
+  // HANDLE SUBMIT
+  document.getElementById('btn-submit').addEventListener('click', async (event) => {
+    event.preventDefault()
+    const code = codeEditor.getValue()
+    const language = document.getElementById('language').value
+    const resultado = document.getElementById('resultado')
+
+    fetch('http://localhost:5000', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ code, language })
+    })
+    .then(data => data.json())
+    .then(result => resultado.innerHTML = result.bot)
+    .catch(err => resultado.innerHTML = err)
+  })
+}
