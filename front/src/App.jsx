cbf29101ba814e74ac08d3c115eb6beeed1a2cc2
KailonFreire <kscarregosa@gmail.com>
2023-06-02 21:51:55 -0300

    Logo readicionada

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,6 +4,7 @@ import QuestionPage from './Components/QuestionPage/QuestionPage';
 import ExerciseList from './Pages/ExerciseList/ExerciseList';
 import LoginPage from './Pages/Auth/LoginPage';
 
+
 function App() {
   const question = 'Given an integer, N , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.'
   
